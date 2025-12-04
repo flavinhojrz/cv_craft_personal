c48e98b529ad7d7b0649c40413b26732e8f9001f
flavinhojrzz_ <flavinhoolvs@gmail.com>
2025-12-04 12:10:07 -0300

    Finalizando web 1

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { OptmizeParams, Resume } from "@/types/types";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
